ea305359af8c6ecd22f8a6eda3d11ec6ca94194e
TeboKhoali <tebohok10@gmail.com>
2023-07-25 00:13:04 -0700

    main header

--- main.h
+++ main.h
@@ -8,5 +8,10 @@
 #define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+int _putchar(char c);
+void print_buffer(char buffer[], int *buff_ind);
+int _itoa(int num, char *buffer, int buff_size);
+
 
 #endif
